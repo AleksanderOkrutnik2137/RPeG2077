9217e5566b002de03c6be8a98946919e8fb542ce
AleksanderOkrutnik2137 <aleksander.iwaniuk@infotech.edu.pl>
2021-11-29 09:23:24 +0100

    "Bugfix" Menu głównego
    Menu w pełni działa.

--- rpg/rpg/Program.cs
+++ rpg/rpg/Program.cs
@@ -20,54 +20,57 @@ namespace rpg
 
         public override void Boot()                     // przeciążenie metody uruchamiania gry
         {
-            bool Bstart;
-            string Start = "Start";
-            string Exit = "Exit";
+            bool Bstart = true;
             int SLength = 2;
             int ELength = 2;
             string[] Sslash = new string[SLength];
             string[] Eslash = new string[ELength];
             Eslash[0] = " ";
-            Sslash[0] = " ";
+            Sslash[0] = ">";
 
             while (Booting == true)
             {
                 Console.Clear();
                 Console.WriteLine("... \n\n\n\n");
-                Console.Write(Sslash[0]);
+
+                Console.Write(Sslash[0]);               // pętle for, zmieniające znaki podczas wybierania
                 for (int i = 1; i < SLength; i++)
                 {
-                    Sslash[i] = Start;
+                    Sslash[i] = "Start";
                     Console.Write(Sslash[i]);
                 }
 
                 Console.Write("\n" + Eslash[0]);
                 for (int i = 1; i < ELength; i++)
                 {
-                    Eslash[i] = Exit;
+                    Eslash[i] = "Exit";
                     Console.Write(Eslash[i]);
                 }
 
                 ConsoleKeyInfo keyinfo = Console.ReadKey(true);
-                if (keyinfo.Key == ConsoleKey.W)
+                if (keyinfo.Key == ConsoleKey.W)        // if-y wybierania opcji
                 {
                     Sslash[0] = ">";
                     Eslash[0] = " ";
                     Bstart = true;
-                    if (keyinfo.Key == ConsoleKey.Enter && Bstart == true)
-                    {
-                        Console.WriteLine("Passed");    // Game.Start
-                    }
                 }
                 else if (keyinfo.Key == ConsoleKey.S)
                 {
                     Eslash[0] = ">";
                     Sslash[0] = " ";
                     Bstart = false;
-                    if (keyinfo.Key == ConsoleKey.Enter && Bstart == false)
-                    {
-                        System.Environment.Exit(0);     // Wyjście z gry
-                    }
+                }
+
+                if (keyinfo.Key == ConsoleKey.Enter && Bstart == true)      // if-y akceptopwania wyboru
+                {
+                    Booting = false;                // zwolnienie pętli
+                    Console.Clear();
+                    Console.WriteLine("Passed");    // Game.Start
+                }
+                else if (keyinfo.Key == ConsoleKey.Enter && Bstart == false)
+                {
+                    Booting = false;
+                    System.Environment.Exit(0);     // Wyjście z gry
                 }
             }
         }
