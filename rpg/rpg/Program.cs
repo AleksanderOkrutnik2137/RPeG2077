680df16d7d34d55f64b7269c4ab078bbbb0d3ec8
AleksanderOkrutnik2137 <aleksander.iwaniuk@infotech.edu.pl>
2022-01-03 21:31:57 +0100

    "RPeG 2077" (kuriozum, z serią zabójstw, po ciężkich walkach)
    Progres w pisaniu gry 69 Poziomu (Kuriozum)
    + Można wybrać klasę (wizjoner lub bandyta)
    + 100% więcej efektów specjalnych.
    + Błogosławieństwo Krzysztofa: niektóre elementy gry będą natychmiastowo dodane wraz z większym progresem (np. Mapa)
    
    - 25% do stabilności psychicznej
    - 75% mniej czasu na ukończenie projektu
    
    > aktywne serie zabójstw
    > połysk: moment of relization

--- rpg/rpg/Program.cs
+++ rpg/rpg/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Threading;
 
+
 namespace rpg
 {
-
-    interface Menu                                      // interfejs do uruchomienia gry
+    interface Menu                  // interfejs do uruchomienia gry
     {
         public abstract void Boot();
     }
@@ -14,12 +14,12 @@ namespace rpg
         public abstract void Boot();
     }
 
-    class Game : MainMenu                               // klasa gry
+    class Game : MainMenu       // klasa gry
     {
-        bool EndGame = false;                           // rozpoczęcie gry i bootup
+        bool EndGame = false;   // rozpoczęcie gry i bootup
         bool Booting = true;
 
-        public override void Boot()                     // przeciążenie metody uruchamiania gry
+        public override void Boot()         // przeciążenie metody uruchamiania gry
         {
             bool Bstart = true;
             int SLength = 2;
@@ -32,9 +32,8 @@ namespace rpg
 
             while (Booting == true)
             {
+                
                 Console.Clear();
-                Console.WriteLine("... \n\n\n\n");
-
                 Console.Write(Sslash[0]);               // pętle for, zmieniające znaki podczas wybierania
                 for (int i = 1; i < SLength; i++)
                 {
@@ -74,16 +73,17 @@ namespace rpg
                     Booting = false;
 
                     Console.Clear();
-                    Random rand = new Random();
+                    Random rand = new Random(); 
                     int index = rand.Next(Ending.Length);
                     string Quote = Ending[index];
                     foreach (char c in Quote)
                     {
                         Console.Write(c);
-                        Thread.Sleep(100);
+                        Thread.Sleep(50);
                     }
                     Thread.Sleep(3000);
-                    System.Environment.Exit(0);     // Wyjście z gry
+                    Environment.Exit(0);     // Wyjście z gry
+                    
                 }
             }
         }
@@ -95,30 +95,126 @@ namespace rpg
         {
             Introduce();
         }
-        private static void Introduce()
+        public static void Introduce()              // metoda rozpoczęcia procesu wprowadzania do gry
         {
-            string mesOne = "Welcome.";
-            string mesTwo = "Welcome to the Survey Program.";
-            string mesThree = "Name your Character...";
-            foreach (char c in mesOne)
+            string[] mes = {"Welcome.", "Welcome to the Survey Program.",};
+            foreach (char c in mes[0])
             {
                 Console.Write(c);
-                Thread.Sleep(100);
+                Thread.Sleep(50);
             }
             Thread.Sleep(2000);
             Console.Clear();
-            foreach (char c in mesTwo)
+            foreach (char c in mes[1])
             {
                 Console.Write(c);
-                Thread.Sleep(100);
+                Thread.Sleep(50);
             }
             Thread.Sleep(2000);
+            Creator.Create();
+        }
+
+    }
+    class Creator           // klasa creatora postaci
+    {
+        bool CreateEnd = false;
+
+        public static void Create()                        
+        {
+            int SLength = 2;
+            int ELength = 2;
+            string[] BanSlash = new string[SLength];
+            string[] WizSlash = new string[ELength];
+            BanSlash[0] = " ";
+            WizSlash[0] = ">";
+            bool Clasa = false;
+            int klasa = 1;                  // 1 = wizjoner, 2 = bandyta
+
+            string[] mes = {"Choose your class...\n\n" };
+
             Console.Clear();
-            foreach(char c in mesThree)
+            foreach (char c in mes[0])
             {
                 Console.Write(c);
-                Thread.Sleep(100);
+                Thread.Sleep(50);
             }
+
+            while (Clasa == false)               // pętle while, zmieniające znaki podczas wybierania
+            {
+                Console.Clear();
+                Console.Write(mes[0]);
+
+                Console.Write(WizSlash[0]);
+                for (int i = 1; i < SLength; i++)
+                {
+                    WizSlash[i] = "Wizjoner";
+                    Console.Write(WizSlash[i]);
+                }
+                Console.Write("\n" + BanSlash[0]);
+                for (int i = 1; i < ELength; i++)
+                {
+                    BanSlash[i] = "Bandyta";
+                    Console.Write(BanSlash[i]);
+                }
+
+                ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+                if (keyinfo.Key == ConsoleKey.W)
+                {
+                    WizSlash[0] = ">";
+                    BanSlash[0] = " ";
+                    klasa = 1;
+                }
+                else if (keyinfo.Key == ConsoleKey.S)
+                {
+                    BanSlash[0] = ">";
+                    WizSlash[0] = " ";
+                    klasa = 2;
+                }
+
+                if (keyinfo.Key == ConsoleKey.Enter && klasa == 1)      // if-y akceptopwania wyboru klasy
+                {
+                    Clasa = true;                
+                    Console.Clear();
+                    Console.WriteLine("passed - wizjoner");             
+                }
+                else if (keyinfo.Key == ConsoleKey.Enter && klasa == 2)
+                {
+                    Clasa = true;
+                    Console.Clear();
+                    Console.WriteLine("passed - bandyta");
+                }
+            }
+        }
+    }
+
+    interface IPlayer                           // klasy gracza
+    {
+
+    }
+    abstract class Player : IPlayer
+    {
+
+    }
+    class Wizjoner : IPlayer
+    {
+        string Name { get; set; }
+        int HeroHP = 10;
+        int HeroATK = 20;
+
+        public void setName(string newName)
+        {
+            Name = newName;
+        }
+    }
+    class Bandyta : IPlayer
+    {
+        string Name { get; set; }
+        int HeroHP = 30;
+        int HeroATK = 17;
+
+        public void setName(string newName)
+        {
+            Name = newName;
         }
     }
 
@@ -126,6 +222,7 @@ namespace rpg
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             Game RPG = new Game();
             RPG.Boot();
         }
